26949289ee35f3e5fbb05dc5a1dbdf5e0f9f60bb
gabriellereyes <ga.reyesa@duocuc.cl>
2023-10-26 23:32:02 -0300

    .

--- controlador.js
+++ controlador.js
@@ -1452,4 +1452,5 @@ router.delete('/EliminarDataCategoriaProducto/:id', async (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,'0.0.0.0', () => {
   console.log(`Servidor en ejecución en el puerto ${PORT}`);
+
 });
